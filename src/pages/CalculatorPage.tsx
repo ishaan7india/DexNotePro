3f7ff461c9c7509a68c325709e1380ac10c4fd79
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-10-28 09:53:34 +0530

    Add CalculatorPage component with Calculator integration

---
+++ src/pages/CalculatorPage.tsx
@@ -0,0 +1,9 @@
+import Calculator from "@/components/Calculator";
+
+export default function CalculatorPage() {
+  return (
+    <div className="min-h-screen bg-background text-foreground p-6">
+      <Calculator />
+    </div>
+  );
+}
