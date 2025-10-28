15607978caca637bb344116cfcd921d80e7f0d04
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-10-28 09:46:02 +0530

    Add ThemeProvider to main.tsx
    Wrap App component in ThemeProvider for theming support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
+// src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider } from "./contexts/ThemeContext"; // 👈 add this import
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider>   {/* 👈 wrap App inside ThemeProvider */}
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
