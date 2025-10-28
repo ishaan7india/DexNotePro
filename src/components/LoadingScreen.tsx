6aa96bf25d69fc8250d2633f81944d2b10649551
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-10-28 13:28:10 +0530

    Add fade class to loading container

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "@/assets/short-logo.png";
-import "./LoadingScreen.css"; // 👈 make sure this CSS file exists
+import "./LoadingScreen.css";
 
 const LoadingScreen = () => {
   return (
-    <div className="loading-container">
+    <div className="loading-container fade">
       <div className="spinner">
         <img src={logo} alt="DexNote" className="loading-logo" />
       </div>
