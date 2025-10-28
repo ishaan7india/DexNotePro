d074c4d9e78d63cf0d06223c6964db0cdeb1d20d
CDT Ishaan <percyjackson7.riptide@gmail.com>
2025-10-28 11:19:34 +0530

    Add LoadingScreen component for loading state

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "@/assets/short-logo.png";
+import "./LoadingScreen.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading-container">
+      <div className="spinner">
+        <img src={logo} alt="DexNote" className="loading-logo" />
+      </div>
+      <p className="loading-text">Loading your DexNote experience...</p>
+    </div>
+  );
+};
+
+export default LoadingScreen;
